79a248ed154c0e4977d6e9a0fd9eba2442f802eb
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-24 19:56:07 +0100

    Added core parser object that's going to run the parsing functions

---
+++ pyon/core/parser.py
@@ -0,0 +1,10 @@
+
+class Parser(object):
+
+    def __init__(self, function, args=[], kwargs={}):
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        return self.function(*self.args, **self.kwargs)
