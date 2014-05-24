145017ad14d412eedfce00153096e50a01d76be6
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-24 20:12:32 +0100

    Added empty parsers.py to app and project templates

---
+++ pyon/conf/project_template/project_name/parsers.py
@@ -0,0 +1,10 @@
+from pyon.core import parser
+
+parsers = [
+#    {
+#        'name': 'all',
+#        'parser': some_parse_function,
+#    }
+    ]
+
+# Create your parsers here, or load some from other measurement apps
