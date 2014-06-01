37f418c86f4395791f1adde6c4eebbc159e923f9
Matt Spraggs <matthew.spraggs@gmail.com>
2014-06-01 18:57:42 +0100

    Added extra logging options to settings.py

--- pyon/core/management/commands/run.py
+++ pyon/core/management/commands/run.py
@@ -6,7 +6,10 @@ import logging
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        logging.basicConfig(level=settings.LOGGING_LEVEL)
+        logging.basicConfig(level=settings.LOGGING_LEVEL,
+                            format=settings.LOGGING_FORMAT,
+                            datefmt=settings.LOGGING_DATEFMT,
+                            filename=settings.LOGGING_FILE)
         project = Project(name=settings.PROJECT_NAME,
                           dump_dir=settings.DUMP_DIR)
         project.main()
