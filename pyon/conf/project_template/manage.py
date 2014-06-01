c51f1c992fd81dc3a51673170e3cd1988e32b700
Matt Spraggs <matthew.spraggs@gmail.com>
2014-06-01 19:07:46 +0100

    Added extra logging options to manage.py and settings.py

--- pyon/conf/project_template/manage.py
+++ pyon/conf/project_template/manage.py
@@ -5,7 +5,10 @@ import logging
 from {{ project_name }} import settings
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
-    logging.basicConfig(level=settings.LOGGING_LEVEL)
+    logging.basicConfig(level=settings.LOGGING_LEVEL,
+                        format=settings.LOGGING_FORMAT,
+                        datefmt=settings.LOGGING_DATEFMT,
+                        filename=settings.LOGGING_FILE)
     from pyon.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
